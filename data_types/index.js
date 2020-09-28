828300f71e6d41b144eadce005d85e2319f969b7
Shreeyasha Baskota <baskotashreeyasha1@gmail.com>
2020-09-28 08:54:12 +0545

    28th August

---
+++ data_types/index.js
@@ -0,0 +1,8 @@
+let first_variable=1,second_variable= "Hello World", boolean_value=false, undefined_value, float_value=2.89;
+console.log("...........");
+console.log(typeof first_variable);
+console.log(typeof second_variable);
+console.log(typeof boolean_value);
+console.log(typeof undefined_value);
+console.log("........");
+console.log(typeof float_value);
