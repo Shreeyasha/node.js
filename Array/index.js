9c28370dcdc0615d8f7046ea9d0d2cdadf69b13b
Shreeyasha Baskota <baskotashreeyasha1@gmail.com>
2020-10-04 09:09:38 +0545

    fourthweek

---
+++ Array/index.js
@@ -0,0 +1,65 @@
+// let fruit1= 'apple', fruit2 = 'mango',fruit3 = 'pears';
+
+// let fruit= ['apple','mango','pears','kiwi'];
+// fruit.pop();
+// fruit.push('avocado');
+// console.log(fruit);
+// fruit.shift();
+// console.log(fruit);
+// fruit.unshift('mushroom');
+// console.log(fruit);
+
+// fruit.splice(1,0,'berry','banana');
+// console.log(fruit);
+
+// fruit.splice(1,2,'berry','banana');
+// console.log(fruit);
+
+
+// fruit='avocado';
+// console.log(fruit)
+
+
+// console.log(typeof fruit[3]);
+// console.log(typeof fruit)
+
+// let my_intro= ['Shreeyasha','Baskota','Chabahil',];
+// console.log(my_intro);
+
+// let newArr = [['hello',2, true], ['iims','bscit'],['kathmandu','pokhara','dharan']];
+// console.log(newArr[2][2]);
+
+// let dc= ['superman','batman',['flash','wonderwoman'],'aquaman'];
+// dc.splice(2,0,'joker')
+// console.log(dc);
+// dc[2].unshift('joker');
+// console.log(dc);
+// or
+// dc[2].splice(0,0,'joker')
+// console.log(dc);
+
+// let dc = ['superman', 'batman', 'aquaman'];
+// let villian =['joker'];
+// console.log(dc.concat(villian));
+
+// dc.splice(3,0,'joker');
+// console.log(dc);
+
+// delete dc[1];
+// console.log(dc);
+
+// let fruits=["Banana","Orange","Lemon","Apple","Mango"];
+// let citrus= fruits.slice(2,3);
+// console.log(fruits);
+// console.log(citrus);
+
+// let dc = ['superman', 'batman', 'aquaman', 'flash','wonderwoman'];
+
+// console.log(dc.slice(3));
+
+let student={};
+student.name='Shreeyasha'
+student.id=1001953476
+student.gender='female'
+student.faculty='IT'
+console.log(student);
--- data_types/index.js
+++ data_types/index.js
@@ -1,4 +1,9 @@
-let first_variable=1,second_variable= "Hello World", boolean_value=false, undefined_value, float_value=2.89;
+let first_variable=1,
+second_variable= "Hello World",
+ boolean_value=false, 
+ undefined_value, 
+ float_value=2.89,
+
 console.log("...........");
 console.log(typeof first_variable);
 console.log(typeof second_variable);
@@ -6,3 +11,5 @@ console.log(typeof boolean_value);
 console.log(typeof undefined_value);
 console.log("........");
 console.log(typeof float_value);
+
+
